f2f58cb73672c82e7b0b7c82f508a230b4016085
Ethan Hanlon <me@ethan-hanlon.xyz>
2022-04-14 11:44:04 -0700

    Added auth signout/localstorage

--- var/www/html/js/authentication.js
+++ var/www/html/js/authentication.js
@@ -1,2 +1,28 @@
 const signin = document.getElementById('signin');
 
+let username = localStorage.getItem('username');
+
+function loginFlow() {
+    let user = document.getElementById('username').value;
+    if (user != '') {
+        localStorage.setItem('username', user);
+        location.reload();
+    }
+}
+
+function signout() {
+    username = '';
+    localStorage.removeItem('username');
+    location.reload();
+}
+
+window.onload = function(_) {
+    if(window.location.href.indexOf('settings') > 0) {
+        username = localStorage.getItem('username');
+        if (username == undefined) {
+            document.getElementById('title').innerHTML = 'Hello, Guest!';
+        } else {
+            document.getElementById('title').innerHTML = 'Hello, ' + username + '!';
+        }
+    }
+}
